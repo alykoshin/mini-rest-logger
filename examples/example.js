243d5143f91f1f749642507b669c3f0b3727f4c7
Alexander <alykoshin@gmail.com>
2016-05-13 00:13:54 +0300

    upd deps

--- examples/example.js
+++ examples/example.js
@@ -15,8 +15,14 @@ var logger2 = require('../')('logger2', {
 
     "winlog_level": "info",
     "winlog_source": "awl-client",
+    "description": [
+      "Configuration for throttling of application messages to EventLog:",
+      "- maxCalls     - max number of calls during observation interval",
+      "- interval     - observation interval",
+      "- warnInterval - interval to output info about the number of rejected messages"
+    ],
 
-    "file_level": "debug",
+  "file_level": "debug",
     "filename": "./log/app.log",
     "maxsize": 1000000,
     "maxFiles": 10
